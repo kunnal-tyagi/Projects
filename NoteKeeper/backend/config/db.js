e9b2ef0bca9418cc7bd6df4e4b11c2fda26043f1
kunnal-tyagi <kunaltyyagi@gmail.com>
2026-08-02 08:24:11 +0530

    Delete NoteKeeper directory

--- NoteKeeper/backend/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require("mongoose")
-
-const ConnectDB=async()=>{
-    const conn=await mongoose.connect(process.env.URL);
-    console.log(`MongoDB connected: ${conn.connection.host} `);
-    
-};
-module.exports=ConnectDB;
--- NoteKeeper/backend/routes/Note.routes.js
+++
@@ -1,7 +0,0 @@
-const router=require('express').Router()
-const {protect}=require('../middleware/authMiddleware')
-const {getNotes,createNote,updateNote,deleteNote}=require('../controllers/NoteContrlo')
-
-router.route('/').get(protect,getNotes).post(protect,createNote);
-router.route('/:id').put(protect,updateNote).delete(protect,deleteNote);
-module.exports=router;
